22c90f1fceb825011c4de27e1e32857605423b3f
Sanjay Muthu Manickam <36176203+SanjayM1995@users.noreply.github.com>
2018-02-08 21:38:15 -0500

    Update Main.java

--- src/edu/buffalo/www/cse4562/Main.java
+++ src/edu/buffalo/www/cse4562/Main.java
@@ -1,6 +1,7 @@
-package edu.buffalo.www.cse4562;
+public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println(" Hello World");
     }
+}
