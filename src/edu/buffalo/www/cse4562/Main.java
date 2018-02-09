069441f24cbc4d83587350aacf7da8dcb48ef2ed
Sanjay Muthu Manickam <36176203+SanjayM1995@users.noreply.github.com>
2018-02-09 16:25:03 -0500

    New

--- src/edu/buffalo/www/cse4562/Main.java
+++ src/edu/buffalo/www/cse4562/Main.java
@@ -4,7 +4,5 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello, World");
-        System.out.println("New");
-        System.out.println("New");
     }
 }
