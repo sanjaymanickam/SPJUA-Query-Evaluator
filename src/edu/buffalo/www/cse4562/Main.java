4949b5127378edd8114b3d9358902fba2a55a53b
Sanjay Muthu Manickam <36176203+SanjayM1995@users.noreply.github.com>
2018-02-08 21:32:29 -0500

    Add files via upload
    Source

---
+++ src/edu/buffalo/www/cse4562/Main.java
@@ -0,0 +1,9 @@
+package edu.buffalo.www.cse4562;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(" Hello World");
+    }
+}
