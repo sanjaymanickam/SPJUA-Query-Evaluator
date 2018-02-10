35142543a8039168df000aca5e9c0a8719f2999b
Sanjay Muthu Manickam <36176203+SanjayM1995@users.noreply.github.com>
2018-02-10 16:05:09 -0500

    Added CCJSQLParser

--- src/edu/buffalo/www/cse4562/Main.java
+++ src/edu/buffalo/www/cse4562/Main.java
@@ -1,9 +1,50 @@
 package edu.buffalo.www.cse4562;
-public class Main {
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import net.sf.jsqlparser.statement.*;
+import net.sf.jsqlparser.parser.CCJSqlParser;
+import net.sf.jsqlparser.parser.ParseException;
+import net.sf.jsqlparser.statement.create.table.CreateTable;
+
+public class Main {
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Hello, World");
-        System.out.println("Sanjay");
+        int i;
+        File dataDir = null;
+        ArrayList<File> sqlFile = new ArrayList<File>();
+        HashMap<String, CreateTable> tables = new HashMap<String, CreateTable>();
+        for (i = 0; i < args.length; i++) {
+            if (args[i].equals("--data")) {
+                dataDir = new File(args[i + 1]);
+                i++;
+            } else {
+                sqlFile.add(new File(args[i]));
+            }
+        }
+        for (File sql : sqlFile) {
+            try {
+                FileReader fil = new FileReader(sql);
+                CCJSqlParser parser = new CCJSqlParser(fil);
+                Statement stmt;
+                while ((stmt = parser.Statement()) != null) {
+                    if (stmt instanceof CreateTable) {
+                        CreateTable ct = (CreateTable) stmt;
+                        tables.put(ct.getTable().getName(), ct);
+                        System.out.println("CreateTable : " + ct.getTable().getName());
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
