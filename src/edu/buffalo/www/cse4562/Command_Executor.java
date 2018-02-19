60d12b950f9086887d28e6193d64d5fc52e530a8
Sanjay Muthu Manickam <36176203+SanjayM1995@users.noreply.github.com>
2018-02-19 18:43:26 -0500

    New

--- src/edu/buffalo/www/cse4562/Command_Executor.java
+++ src/edu/buffalo/www/cse4562/Command_Executor.java
@@ -26,7 +26,7 @@ public class Command_Executor {
 
                     // project here
                     while((stmt = parser.Statement()) != null){
-                        System.out.println(stmt);
+//                        System.out.println(stmt);
                     Visitor_Parse stmt_visitor = new Visitor_Parse();
                     stmt.accept(stmt_visitor);
                     System.out.println(prompt);
