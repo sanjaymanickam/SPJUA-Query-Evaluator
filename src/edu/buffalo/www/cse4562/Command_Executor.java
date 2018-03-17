c37e701b414a911deed04663fd0292c8394b2737
Bharadwaj <bparthas@buffalo.edu>
2018-03-17 16:52:00 -0400

    Query 1 order by fixed

--- src/edu/buffalo/www/cse4562/Command_Executor.java
+++ src/edu/buffalo/www/cse4562/Command_Executor.java
@@ -136,11 +136,12 @@ public class Command_Executor {
                         String temp = Data_Storage.tables.get(new_col.getTable().getName()).get(new_col.getColumnName());
                         if(temp.equals("DOUBLE"))
                         {
-                            System.out.print(new DoubleValue(itr.next()));
+                            DoubleValue d_value = new DoubleValue(itr.next());
+                            System.out.print(d_value);
                         }
-                        else if(temp.equals("STRING"))
+                        else if(temp.equals("STRING") || temp.equals("VARCHAR") || temp.equals("CHARACTER"))
                         {
-                            System.out.print(new StringValue(itr.next()));
+                            System.out.print(new StringValue(itr.next().toString()));
                         }
                         else {
                             System.out.print(itr.next());
