88b7a608a84594e176c0a5406c1c475d0a96dbbe
msanj <msanjay0695@gmail.com>
2018-04-13 16:37:30 -0400

    New Optimization Fix

--- src/edu/buffalo/www/cse4562/ProjectionIterator_Interface.java
+++ src/edu/buffalo/www/cse4562/ProjectionIterator_Interface.java
@@ -28,55 +28,59 @@ public class ProjectionIterator_Interface implements Iterator_Interface{
             tup = iter.readOneTuple();
         String tableName = null, colName;
         if(tup!=null) {
-                Iterator project_iter = selectedColumns.iterator();
-                while (project_iter.hasNext()) {
-                    colName = project_iter.next().toString();
-                    String old_val = new String();
-                    old_val = old_val+(colName);
-                    if (colName.indexOf(".") != -1) {
-                        col = Data_Storage.stringSplitter(colName);
-                        tableName = col.getTable().getName();
-                        colName = col.getColumnName();
-                    }
-                    if (Data_Storage.alias_table.containsValue(colName)) {
-                        Set set = Data_Storage.alias_table.entrySet();
-                        Iterator set_iterator = set.iterator();
-                        while (set_iterator.hasNext()) {
-                            Map.Entry entry = (Map.Entry) set_iterator.next();
-                            if (entry.getValue().equals(colName)) {
-                                origColName = entry.getKey().toString();
-                            }
-
-                        }
-                        aliasColName = colName;
-                        if (origColName.indexOf(".") != -1) {
-                            col = Data_Storage.stringSplitter(origColName);
-                            origTableName = col.getTable().getName();
-                            origColName = col.getColumnName();
-                        }
-                    }
-                    else if(Data_Storage.alias_table.containsKey(old_val)){
-                        aliasColName = Data_Storage.alias_table.get(old_val);
-                    }
-                    else {
-                        origColName = colName;
-                        aliasColName = origColName;
-                    }
-                    if (Data_Storage.table_alias.containsKey(origTableName)) {
-                        aliasTableName = origTableName;
-                        origTableName = Data_Storage.table_alias.get(origTableName);
-                    } else {
-                        aliasTableName = null;
-                        origTableName = Data_Storage.current_schema.get(colName);
-                    }
-                    int position = tup.schema.indexOf(new Column(new Table(aliasTableName), origColName));
-                    if (position != -1) {
-                          tuple.add(tup.tuples.get(position));
-                          schema.add(Data_Storage.stringSplitter(old_val));
-                    }
-                }
-            return new Tuple(tuple,schema);
-//                return tup;
+//                Iterator project_iter = selectedColumns.iterator();
+//                while (project_iter.hasNext()) {
+//                    colName = project_iter.next().toString();
+//                    String old_val = new String();
+//                    old_val = old_val+(colName);
+//                    if (colName.indexOf(".") != -1) {
+//                        col = Data_Storage.stringSplitter(colName);
+//                        tableName = col.getTable().getName();
+//                        colName = col.getColumnName();
+//                    }
+//                    if (Data_Storage.alias_table.containsValue(colName)) {
+//                        Set set = Data_Storage.alias_table.entrySet();
+//                        Iterator set_iterator = set.iterator();
+//                        while (set_iterator.hasNext()) {
+//                            Map.Entry entry = (Map.Entry) set_iterator.next();
+//                            if (entry.getValue().equals(colName)) {
+//                                origColName = entry.getKey().toString();
+//                            }
+//
+//                        }
+//                        aliasColName = colName;
+//                        if (origColName.indexOf(".") != -1) {
+//                            col = Data_Storage.stringSplitter(origColName);
+//                            origTableName = col.getTable().getName();
+//                            origColName = col.getColumnName();
+//                        }
+//                    }
+//                    else if(Data_Storage.alias_table.containsKey(old_val)){
+//                        aliasColName = Data_Storage.alias_table.get(old_val);
+//                    }
+//                    else {
+//                        origColName = colName;
+//                        aliasColName = origColName;
+//                    }
+//                    if (Data_Storage.table_alias.containsKey(origTableName)) {
+//                        aliasTableName = origTableName;
+//                        origTableName = Data_Storage.table_alias.get(origTableName);
+//                    } else {
+//                        aliasTableName = null;
+//                        origTableName = Data_Storage.current_schema.get(colName);
+//                    }
+//                    if(Data_Storage.from_alias!= null)
+//                    {
+//                        aliasTableName = Data_Storage.from_alias;
+//                    }
+//                    int position = tup.schema.indexOf(new Column(new Table(origTableName), origColName));
+//                    if (position != -1) {
+//                          tuple.add(tup.tuples.get(position));
+//                          schema.add(Data_Storage.stringSplitter(old_val));
+//                    }
+//                }
+//            return new Tuple(tuple,schema);
+                return tup;
         }
         else {
                 return null;
