be73d2514cedc09d7cff34ed01727ef241a332c7
msanj <msanjay0695@gmail.com>
2018-04-13 11:36:31 -0400

    Fixed join

--- src/edu/buffalo/www/cse4562/Command_Executor.java
+++ src/edu/buffalo/www/cse4562/Command_Executor.java
@@ -44,12 +44,12 @@ public class Command_Executor {
                 Visitor_Parse.ret_type(stmt);
                 if(Data_Storage.oper!=null) {
                     System.err.println(count);
-                    if(count < 3){
-                        System.err.println("query1 over");
-                        System.out.println(prompt);
-                        count++;
-                        continue;
-                    }
+//                    if(count < 3){
+//                        System.err.println("query1 over");
+//                        System.out.println(prompt);
+//                        count++;
+//                        continue;
+//                    }
                     if(Data_Storage.join ==1) {
                         Iterator_Interface iter = new Optimize().optimize(Data_Storage.oper);
                         Data_Storage.oper = iter;
