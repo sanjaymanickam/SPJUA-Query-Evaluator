4d9a327193b4c951a8493f6c8319650c84e39407
Bharadwaj <bparthas@buffalo.edu>
2018-04-15 15:07:01 -0400

    test ref impl

--- src/edu/buffalo/www/cse4562/Main.java
+++ src/edu/buffalo/www/cse4562/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         try {
         FileInputStream is = new FileInputStream(new File("1.txt"));
-        System.setIn(is);
+System.setIn(is);
             Command_Executor.exec(args);
         } catch (Exception e) {
             e.printStackTrace();
