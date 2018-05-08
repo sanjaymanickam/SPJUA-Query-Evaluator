bcad28c4d15b8ac8a7c13508b67826e07563b794
Bharadwaj <bparthas@buffalo.edu>
2018-05-08 13:26:37 -0400

    stats

--- src/edu/buffalo/www/cse4562/Preprocess.java
+++ src/edu/buffalo/www/cse4562/Preprocess.java
@@ -20,9 +20,30 @@ public class Preprocess {
         HashSet<String> files = new HashSet<>();
         try {
             int tupleCount = 0;
+            int retFlag = 0;
+            int mkt = 0;
+            int ship = 0;
             BufferedReader buf = new BufferedReader(new FileReader(new File("data/"+file+".dat")));
             String line = buf.readLine();
             while (line !=null){
+                ArrayList<String> tuple = new ArrayList<>();
+                StringTokenizer stringTokenizer = new StringTokenizer(line,"|");
+                    while (stringTokenizer.hasMoreElements()){
+                        tuple.add(stringTokenizer.nextElement().toString());
+                    }
+                if(file.equals("LINEITEM")){
+                    if(tuple.get(8).equals("R")){
+                        retFlag++;
+                    }
+                    if(tuple.get(13).equals("DELIVER IN PERSON")){
+                        ship++;
+                    }
+                }
+                else if(file.equals("CUSTOMER")){
+                        if(tuple.get(6).equals("BUILDING")){
+                            mkt++;
+                        }
+                }
 //                for(ArrayList<String> keys : indexMeta){
 //                    int pos = Integer.parseInt(keys.get(1));
 //                    String colName = keys.get(0);
@@ -43,6 +64,9 @@ public class Preprocess {
                 line = buf.readLine();
             }
             System.err.println("TableName : "+file+" ------- "+tupleCount);
+            System.err.println("Return Flag : "+file+" ------- "+retFlag);
+            System.err.println("Mkt : "+file+" ------- "+mkt);
+            System.err.println("Shipinst : "+file+" ------- "+ship);
         }
         catch (FileNotFoundException e){
             e.printStackTrace();
